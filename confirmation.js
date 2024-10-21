a704e76a5512dbe7273e951838bfc0c5920e016e
Mary Nguyen <mpn64894@uga.edu>
2024-10-20 22:16:27 -0400

    sos

--- confirmation.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Example function to handle printing confirmation
-    document.getElementById('print-button').addEventListener('click', function () {
-        window.print();
-    });
-});
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-// next.config.mjs
-const nextConfig = {
-    images: {
-      domains: ['resizing.flixster.com', 'i.ebayimg.com', "m.media-amazon.com"],
-    },
-  };
-  
-  export default nextConfig;
-  
