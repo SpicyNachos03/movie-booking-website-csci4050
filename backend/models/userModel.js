5481e0b581b491232d988c27f935479c3d1d7310
Joseph <60304239+josephvu10@users.noreply.github.com>
2024-10-22 19:25:18 -0400

    fixed extra line and comma

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
@@ -9,7 +8,7 @@ const userSchema = new mongoose.Schema({
   billingAddress: { type: String }, // Add this field
   promotions: { type: Boolean, default: false },
   cards: { type: [String], validate: [val => val.length <= 4, 'Max 4 cards allowed'] }, // Limit 4 cards
-  status: { type: String, enum: ['active', 'inactive'], default: 'active' },,
+  status: { type: String, enum: ['active', 'inactive'], default: 'active' },
   password: { type: String, required: true }
 });
 
