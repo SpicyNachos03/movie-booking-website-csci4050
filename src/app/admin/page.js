575b8526ab7654bcb76b88b2f2d15a191365f12a
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2024-11-17 15:40:05 -0500

    Updated Admin Page

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -6,6 +6,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { Button } from 'react-bootstrap';
 import Link from 'next/link';
+import './admin.css';
 
 /* 
     Don't need to touch until Sprint 3
@@ -15,21 +16,19 @@ function Admin(){
     return(
         <div>
             <Header></Header>
-            <h1>This is the admin page</h1>
-            <p>We need options to manage:
-                -movies
-                -users
-                -promotions
-            </p>
-            <p>Add links to access the manage movies, manage users, and manage promotions screens</p>
-            <div>
-                <Link href="/admin/manageMovies" className="text-white hover:text-lightCyan">Manage Movies</Link>
-            </div>
-            <div>
-                <Link href="/admin/manageUsers" className="text-white hover:text-lightCyan">Manage Users</Link>
-            </div>
-            <div>
-                <Link href="/admin/managePromotions" className="text-white hover:text-lightCyan">Manage Promotions</Link>
+            <div className="adminBox">
+                <h1>This is the admin page</h1>
+
+                <p>Add links to access the manage movies, manage users, and manage promotions screens</p>
+                <div>
+                    <Link href="/admin/manageMovies" className="text-white hover:text-lightCyan">Manage Movies</Link>
+                </div>
+                <div>
+                    <Link href="/admin/manageUsers" className="text-white hover:text-lightCyan">Manage Users</Link>
+                </div>
+                <div>
+                    <Link href="/admin/managePromotions" className="text-white hover:text-lightCyan">Manage Promotions</Link>
+                </div>
             </div>
             <Footer></Footer>
         </div>
