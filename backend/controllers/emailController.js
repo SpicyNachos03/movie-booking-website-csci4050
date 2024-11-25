554ebfdc9d4b2249549084df675e425038a89a4b
tminning <tmoth@uga.edu>
2024-11-24 22:15:29 -0500

    promotion emails now contain name and discount of promotion

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -2,6 +2,7 @@ const nodemailer = require('nodemailer');
 const movieDB = require('../models/movieModel.js');
 //const userDB = require('../models/userModel.js');
 const userDB = require('../models/userModel.js');
+const promotionDB = require('../models/promotionModel.js');
 
 
 async function sendPromotionNotificationEmail() {
@@ -14,13 +15,21 @@ async function sendPromotionNotificationEmail() {
       return;
     }
 
+    // Fetch the latest promotion
+    const latestPromotion = await promotionDB.findOne().sort({ _id: -1 }); // Sort by ID to get the latest entry
+    if (!latestPromotion) {
+        console.log('No promotions found in the database.');
+        return;
+    }
+    const { promotionName, promotionRate } = latestPromotion;
+
     for (const user of subscribedUsers) {
       try {
         let emailInfo = await emailTransporter.sendMail({
           from: '"Movie Booking" <moviebookingcsci4050a7@gmail.com>',
           to: user.email, // Access the email field from the User model
           subject: 'Notification of promotion',
-          text: `Dear ${user.firstName},\n\nLog in to your profile to check out new promotions available to you now!\n\nBest regards,\nMovie Booking Team`,
+          text: `Dear ${user.firstName},\n\nWe are excited to announce a new promotion just for you!\n\nPromotion: ${promotionName}\nDiscount: ${promotionRate}% off\n\nLog in to your profile now to take advantage of this amazing deal.\n\nBest regards,\nMovie Booking Team`,
         });
 
         console.log(`Email sent to ${user.email}:`, emailInfo.messageId);
