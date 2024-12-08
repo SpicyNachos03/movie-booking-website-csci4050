10de78fb9071f9bdd5c96e495b756c805a9fd394
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2024-12-08 09:54:10 -0500

    Model Update

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -1,22 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Show = require('./showModel');
+
+const ticketSchema = new mongoose.Schema({
+    seatName: {
+        type: String,
+        required: true
+    },
+    ticketType: {
+        type: String,
+        enum: ['Adult', 'Child', 'Senior'],
+        default: 'Adult',
+        required: true
+    }
+});
 
 const bookingSchema = new mongoose.Schema({
     bookingNumber: {
         type: String,
         required: true,
     },
-    ticketNumbers: {
-        type: [String],
-        required: true,
-    },
-    movieId: {
-        type: movieId,
-        required: true,
-    },
-    showInformation: {
-        type: String,
-        required: true,
-    },
+
+    ticketArray: [ticketSchema],
+
+    showInformation: Show,
+
+    orderTotal: {
+        type: Number,
+        required: true
+    }
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
